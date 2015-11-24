13792231ea5e623ee3d3a34fc112f3cbfa513c40
ntodosiychuk <nata.todosiy@gmail.com>
2015-11-24 13:13:55 +0000

    Updated license file by MIT license content.

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from setuptools import setup
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
-# allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
@@ -12,17 +11,16 @@ setup(
     version='0.1',
     packages=['view-acl'],
     include_package_data=True,
-    license='MIT',  # example license
+    license='Apache License',
     description='Views ACL plugin is a Django app to provide method for automatic permissions generation based on urlpatterns in urls.py',
     long_description=README,
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache License', # example license
+        'License :: OSI Approved :: Apache License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        # Replace these appropriately if you are stuck on Python 2.
         'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
